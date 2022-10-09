295e5fa95e816f8795bf7356af133c3476fa67da
fujisii <51264835+fujisii@users.noreply.github.com>
2022-10-09 15:05:00 +0900

    37. Event driven（イベントドリブン）とは？

--- web-server/index.js
+++ web-server/index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.send("トップページ");
+});
+
+app.get('/about', function (req, res) {
+  res.send("Aboutページ");
+});
 
 app.listen(3000)
