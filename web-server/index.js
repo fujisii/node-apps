2098e38afe3cbd299fb8ceaa3851b664664a5e1a
fujisii <51264835+fujisii@users.noreply.github.com>
2022-10-10 18:21:57 +0900

    39. HTMLやJSONを返す方法

--- web-server/index.js
+++ web-server/index.js
@@ -2,15 +2,16 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send("トップページ");
+  res.send("<h1>トップページ</h1>");
 });
 
-app.get('/about', function (req, res) {
-  res.send("Aboutページ");
+app.get('/api/v1/users', function (req, res) {
+  res.send({
+    name: "Mike",
+    age: 30
+  });
 });
 
 app.listen(3000, function(){
   console.log("I am running!");
 });
-
-console.log("最終行");
