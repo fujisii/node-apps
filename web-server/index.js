cba85add25408beb45dc201c0854a57fa0f7c863
fujisii <51264835+fujisii@users.noreply.github.com>
2022-10-11 18:18:50 +0900

    49. フォーム情報をバックエンドに送信する方法

--- web-server/index.js
+++ web-server/index.js
@@ -2,12 +2,20 @@ const express = require('express');
 const path = require("path");
 const app = express();
 
+// postでbodyを扱えるようにする
+app.use(express.urlencoded({ extended: false }));
+// publicディレクトリの指定
 app.use(express.static(path.join(__dirname, "public")));
 
 // app.get('/', function (req, res) {
 //   console.log(req);
 //   res.send("<h1>トップページ！！</h1>");
 // });
+
+app.post('/api/v1/quiz', function (req, res) {
+  const answer = req.body.answer;
+  res.send(answer);
+});
 
 app.get('/api/v1/users', function (req, res) {
   res.send({
