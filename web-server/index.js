67e2af58c089ad7fb61788d53d4516f0464fe7dc
fujisii <51264835+fujisii@users.noreply.github.com>
2022-10-11 17:19:51 +0900

    44. HTMLファイルを読み込めるようにする方法

--- web-server/index.js
+++ web-server/index.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require("path");
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send("<h1>トップページ！！</h1>");
-});
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.get('/', function (req, res) {
+//   console.log(req);
+//   res.send("<h1>トップページ！！</h1>");
+// });
 
 app.get('/api/v1/users', function (req, res) {
-  console.log(req);
   res.send({
     name: "Mike",
     age: 30
